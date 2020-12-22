5a7987ad04237c6952d7943850f4746983dfb638
donchodonev <doniodonef@gmail.com>
2020-12-22 10:49:44 +0200

    All solutions from First Steps in Coding from the CSharp Programming Basics course

---
+++ BirthdayParty/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BirthdayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hall_rent = double.Parse(Console.ReadLine());
+            double cake_price = hall_rent / 5;
+            double drinks_price = cake_price * 0.55;
+            double animator_price = hall_rent / 3;
+
+            double total = hall_rent + cake_price + drinks_price + animator_price;
+
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ DepositCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit_sum = double.Parse(Console.ReadLine());
+            int period = int.Parse(Console.ReadLine());
+            double yearly_interest_rate = double.Parse(Console.ReadLine()) / 100;
+
+            double sum = deposit_sum + period * ((deposit_sum * yearly_interest_rate) / 12);
+
+
+            Console.WriteLine(sum);
+        }
+    }
+}
