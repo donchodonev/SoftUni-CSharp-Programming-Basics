6543eb5f1db74699140c30bdcd5c711734087f80
donchodonev <doniodonef@gmail.com>
2020-12-22 14:33:53 +0200

    adding for loops problems' solutions

---
+++ ForLoops/CharacterSequence/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CharacterSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int length = text.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                char letter = text[i];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
---
+++ ForLoops/SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCount = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
