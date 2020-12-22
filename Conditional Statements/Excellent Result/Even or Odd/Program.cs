17e31e58bdce524d3da0e1b2de807bee4989daf0
donchodonev <doniodonef@gmail.com>
2020-12-22 11:35:59 +0200

    conditional statement solutions added

---
+++ Conditional Statements/Excellent Result/Even or Odd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade = int.Parse(Console.ReadLine());
+
+            if (grade % 2 != 0)
+            {
+                Console.WriteLine("odd");
+            }
+            else
+            {
+                Console.WriteLine("even");
+            }
+        }
+    }
+}
